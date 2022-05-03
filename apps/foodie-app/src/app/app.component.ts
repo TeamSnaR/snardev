6afa9db3cc75e566475ef9c64ea1eaf77881ff92
lhar gil <6247364+lhargil@users.noreply.github.com>
2022-05-03 15:49:07 +0800

    feat: added foodie-app

---
+++ apps/foodie-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'snardev-root',
+  template: ` <snardev-nx-welcome></snardev-nx-welcome> `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'foodie-app';
+}
