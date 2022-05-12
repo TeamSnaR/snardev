c295b8c66b805244eea5838b0a65957089177aae
lhar gil <6247364+lhargil@users.noreply.github.com>
2022-05-13 06:17:04 +0800

    feat: added initial split-form ui

--- apps/split-app/src/app/app.module.ts
+++ apps/split-app/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { SplitFormComponent } from './split-form.component';
+import { SplitFormUiComponent } from './split-form-ui.component';
 
 @NgModule({
-  declarations: [AppComponent, SplitFormComponent],
+  declarations: [AppComponent, SplitFormComponent, SplitFormUiComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
