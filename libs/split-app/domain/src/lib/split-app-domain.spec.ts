eb6aa11f9602302f19e68b7ec8e1ec41193bc394
lhar gil <6247364+lhargil@users.noreply.github.com>
2022-06-06 16:50:52 +0100

    chore: remove unused test

--- libs/split-app/domain/src/lib/split-app-domain.spec.ts
+++
@@ -1,7 +0,0 @@
-import { splitAppDomain } from './split-app-domain';
-
-describe('splitAppDomain', () => {
-  it('should work', () => {
-    expect(splitAppDomain()).toEqual('split-app-domain');
-  });
-});
